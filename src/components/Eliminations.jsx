c29aa91fdd79a78e8e63acca1565d7cc1a4123e5
Eyup <171042578+Eyup-16@users.noreply.github.com>
2025-02-12 12:30:56 +0100

    Eliminations.jsx
    The Last Official Code.

--- src/components/Eliminations.jsx
+++ src/components/Eliminations.jsx
@@ -1,16 +1,25 @@
-import {useState} from 'react'
 import { languages } from '../languages'
-import {nanoid} from 'nanoid'
-const language = languages.map((lang)=>{
- return <span className='lang' key={nanoid()} style={{backgroundColor:lang.backgroundColor,color:lang.color}}>{lang.name}</span>
-})
-
+import clsx from 'clsx'
+import { getFarewellText } from '../utilis'
 
 function Eliminations(props) {
+  const lastEliminated = props.mistakes > 0 ? languages[props.mistakes - 1] : null;
+  const fareWellText = lastEliminated && props.isLastGuessIncorrect ? getFarewellText(lastEliminated.name) : "";  
+  const noticeClass =clsx('notice',{
+    correct:props.won,
+    incorrect:props.lost,
+    farewell:!props.gameOver && props.isLastGuessIncorrect
+  })
+
+  const language = languages.map((lang,index)=>{
+    return <span className={index+1 <= props.mistakes?'lang lost':'lang'} key={lang.name} style={{backgroundColor:lang.backgroundColor,color:lang.color}}>{lang.name}</span>
+  })
+  // We need to design the fareWellText
   return (
     <>
-    <section className="notice">
-        <p>"FareWell HTML & CSS"🫡</p>
+    <section className={noticeClass}>
+        <h1>{props.lost? 'Game over!': props.won? 'You Win!':null}</h1>
+        <p>{props.lost?'You lose! Better start learning Assembly 😭':props.won?'Well Done🎉':fareWellText}</p>
     </section>
     <section className='eliminations'>
       {language}
